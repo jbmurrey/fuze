3fc7aac14e9ff9e35543dab3b3fad8381fbe75fa
JB_Murrey <jonahmurrey@yahoo.com>
2021-12-20 18:31:15 -0600

    https://github.com/jbmurrey/fuze

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron')
+
+    let mainWindow
+
+    function createWindow () {
+      mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true
+        }
+      })
+      const url = '../fuze/dist/fuze/index.html'
+      mainWindow.loadURL("http://localhost:4200/")
+      mainWindow.webContents.openDevTools()
+
+      mainWindow.on('closed', function () {
+        mainWindow = null
+      })
+    }
+
+    app.on('ready', createWindow)
+
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') app.quit()
+    })
+
+    app.on('activate', function () {
+      if (mainWindow === null) createWindow()
+    })
